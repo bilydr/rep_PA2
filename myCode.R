c3249bbddadd81a3c34e6578f1a82beef282d982
bilydr <bilongyi@gmail.com>
2015-02-19 23:24:37 +0100

    new commit

--- myCode.R
+++ myCode.R
@@ -1,1 +1,11 @@
+rm(list=ls())
+
 library(dplyr)
+library(ggplot2)
+
+dfStorm <-read.csv("./data/repdata-data-StormData.csv",
+                   stringsAsFactors = F)
+dfStorm <-tbl_df(dfStorm)
+
+dfStorm <-select(dfStorm, 
+                 EVTYPE, FATALITIES, INJURIES, CROPDMG, PROPDMG)
